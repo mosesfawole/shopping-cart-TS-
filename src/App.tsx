92d5161def8c4340cdd1d34e266ffbf9514be0b7
Moses Fawole <mosesfawole2001@gmail.com>
2023-01-19 08:12:28 +0100

    initial setup app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { useState } from "react";
+import { useQuery } from "react-query";
+
+// components
+import Drawer from "@material-ui/core/Drawer";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Grid from "@material-ui/core/Grid";
+import AddShoppingCart from "@material-ui/icons/AddShoppingCart";
+import Badge from "@material-ui/core/Badge";
+
+// styles
+
+const App = () => {
+  return <div className=""></div>;
+};
 
 export default App;
