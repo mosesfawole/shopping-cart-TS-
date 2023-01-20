7de0e426594e8e517bf88a2963c2eabb9dd64437
Moses Fawole <mosesfawole2001@gmail.com>
2023-01-20 23:31:28 +0100

    initial cart component

---
+++ src/components/CartItem/CartItem.tsx
@@ -0,0 +1,13 @@
+import Button from "@material-ui/core/Button";
+
+// Types
+import { CartItemType } from "../../App";
+
+// Styles
+import { Wrapper } from "./CartItem.styles";
+
+const CartItem = () => {
+  return <div>CartItem</div>;
+};
+
+export default CartItem;
